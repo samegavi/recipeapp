f55e19c039dbcbaed1a9ce5acaf25ebc1ba319fe
Samuel Amegavi <samegavi@gmail.com>
2023-07-17 08:32:29 +0000

    add static_root path

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -123,6 +123,12 @@ STATICFILES_DIRS=[
 ]
 
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT= BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
